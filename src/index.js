d64cbf95e642a0134ab19d1ee0008c131195c2be
Ayan Banerjee <ayanbanerjee102@gmail.com>
2023-01-23 21:24:09 +0530

    Completed the undo bar progress animation

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 
 const form = document.querySelector("form");
 const inputField = document.querySelector(" form input");
-const progressBar = document.querySelector(".progressBar");
-const progressBarDiv = progressBar.querySelector("div");
+const progressBar = document.querySelector(".round-time-bar");
 
 let formValues = {};
 
@@ -23,19 +22,13 @@ function getFormValues(e) {
   formValues = { ...formValues, [e.target.name]: e.target.value };
 }
 
-function progress(timeleft, timetotal, element) {
-  var progressBarWidth = (timeleft * element.width()) / timetotal;
-  progressBarDiv
-    .animate({ width: progressBarWidth }, 500)
-    .html(Math.floor(timeleft / 60) + ":" + (timeleft % 60));
-  if (timeleft > 0) {
-    setTimeout(function () {
-      progress(timeleft - 1, timetotal, element);
-    }, 1000);
-  }
+function UndoAnimation() {
+  progressBar.classList.remove("round-time-bar");
+  progressBar.offsetWidth;
+  progressBar.classList.add("round-time-bar");
 }
 
-progress(30, 30, progressBar);
+UndoAnimation();
 
 // Add Event Listeners
 form.addEventListener("submit", handleSubmit);
