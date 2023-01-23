ee1e4e2dbea10b2461571484a090b418cb8c8d10
Ayan Banerjee <ayanbanerjee102@gmail.com>
2023-01-23 12:56:53 +0530

    Initial Commit

---
+++ webpack.dev.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const common = require("./webpack.common");
+const { merge } = require("webpack-merge");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = merge(common, {
+  mode: "development",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    port: 8080,
+    hot: false,
+    liveReload: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+});
